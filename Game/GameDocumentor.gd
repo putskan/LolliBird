f1b747f448c975dbf39500e27b65062c5f015244
putskan <mule9003@gmail.com>
2020-12-19 00:06:10 +0200

    Add GameDocumentor, PlayerData objects

---
+++ Game/GameDocumentor.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+var current_round
+var total_rounds
+
+# consists players (PlayerData) for each team
+var players = {
+	1: [],
+	2: []
+}
+
+func _ready():
+	pass
---
+++ Game/src/Game/PlayerData.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+# used in server side for each player
+var player_name
+var player_id
+var team
+var is_captive = false
+var captives = []
